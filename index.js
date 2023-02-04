ff65dae5d4f968028477c672afb4b81a3901ca74
Faiz Ghanchi <87257423+faiz-gh@users.noreply.github.com>
2023-02-05 04:36:27 +0530

    Setup Firebase Hosting

---
+++ index.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrJw3sMkveH89z6qRvlzxw2L2vwaCKEW4",
+  authDomain: "monke-inventory.firebaseapp.com",
+  projectId: "monke-inventory",
+  storageBucket: "monke-inventory.appspot.com",
+  messagingSenderId: "281878113432",
+  appId: "1:281878113432:web:1333912e11a91a4476b4ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
