070e4480b2d4345b5bcbe0c540bf3ec9b9cf9d4e
Faiz Ghanchi <87257423+faiz-gh@users.noreply.github.com>
2023-02-05 16:34:00 +0530

    added upload functionality

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+const firebase = require('firebase/app'); // Importing the Firebase App
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,4 +11,38 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+
+// Upload Button Functionality
+function uploadButton() {
+  document.getElementById('imageInput').click();
+}
+
+const form = document.getElementById('imageUploadForm');
+const fileInput = document.getElementById('imageInput');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('Submitting form');
+  const file = fileInput.files[0];
+  
+  const formData = new FormData();
+  formData.append('file', file);
+  
+  fetch('http://43.205.216.220:4200/inventoryUpload', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Failed to upload file');
+  })
+  .then(data => {
+    console.log('File was uploaded successfully', data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+});
