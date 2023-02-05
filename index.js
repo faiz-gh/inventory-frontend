a0c08d07b1a6ef52c2ce08212314e7444adc498e
Faiz Ghanchi <87257423+faiz-gh@users.noreply.github.com>
2023-02-05 17:15:36 +0530

    improved ui design

--- index.js
+++ index.js
@@ -12,37 +12,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Upload Button Functionality
-function uploadButton() {
-  document.getElementById('imageInput').click();
-}
-
-const form = document.getElementById('imageUploadForm');
-const fileInput = document.getElementById('imageInput');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  console.log('Submitting form');
-  const file = fileInput.files[0];
-  
-  const formData = new FormData();
-  formData.append('file', file);
-  
-  fetch('http://43.205.216.220:4200/inventoryUpload', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error('Failed to upload file');
-  })
-  .then(data => {
-    console.log('File was uploaded successfully', data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
-});
