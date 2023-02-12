fafefe6a9f456a9eda1278a48e751fd7995fda33
Faiz Ghanchi <faizghanchi1278@gmail.com>
2023-02-13 03:35:20 +0530

    Everything ready except bill details page

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc, onSnapshot } from "firebase/firestore";
+import { getFirestore, collection, getDocs, getDoc, doc, getCountFromServer } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,14 +18,24 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Index Page
-export function indexPage(){
+export async function indexPage(){
     const totalBills = document.querySelector("#main-wrapper > div.content-body > div > div.row > div:nth-child(1) > div > div > div > div > div > h2");
     const totalAmount = document.querySelector("#main-wrapper > div.content-body > div > div.row > div:nth-child(2) > div > div > div > div > div > h2");
+    if (totalBills != null && totalAmount != null){
+        const coll = collection(db, "bills");
+        const snapshot = await getCountFromServer(coll);
+        totalBills.innerHTML = snapshot.data().count;
 
-    onSnapshot(doc(db, "data", "stats"), (doc) => {
-        totalBills.innerHTML = doc.data().totalBills;
-        totalAmount.innerHTML = doc.data().totalAmount;
-    });
+        var totalAmt = 0;
+        const querySnapshot = await getDocs(collection(db, "bills"));
+        querySnapshot.forEach((doc) => {
+            var invoiceTotal = doc.data().total;
+            var number = parseFloat(invoiceTotal.replace(/[^\d.]/g, ''));
+            totalAmt += number;
+            console.log(number);
+        });
+        totalAmount.innerHTML = totalAmt;
+    }
 
     // Upload Image
     const form = document.getElementById("image-form");
@@ -50,7 +60,7 @@ export async function billsPage(){
     if(document.querySelector("#example4 > tbody > tr.odd")){
         document.querySelector("#example4 > tbody > tr.odd").remove();
     }
-
+    
     const querySnapshot = await getDocs(collection(db, "bills"));
 
     querySnapshot.forEach((doc) => {
@@ -68,11 +78,13 @@ export async function billsPage(){
                 <td>${vendorPhone}</td>
                 <td>${invoiceTotal}</td>
                 <td>
-                <button onclick="alert('Show Details feature is under development!')" type="button" class="btn btn-rounded btn-secondary">
-                        <span class="btn-icon-left text-secondary">
-                            <i class="fa fa-eye color-secondary"></i>
-                        </span>
-                    </button>
+                    <a href="bill-details.html?id=${docId}">
+                        <button type="button" class="btn btn-rounded btn-secondary">
+                            <span class="btn-icon-left text-secondary">
+                                <i class="fa fa-eye color-secondary"></i>
+                            </span>
+                        </button>
+                    </a>
                 </td>
             `;
         }
@@ -80,6 +92,22 @@ export async function billsPage(){
 }
 
 // Bill Details Page
-export function billDetailsPage(){
-    console.log("Bill Details Page Loaded");
+export async function billDetailsPage(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const docId = urlParams.get("id");
+
+    const docRef = doc(db, "bills", docId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        const invoiceId = docSnap.data().invoice_id;
+        const invoiceDate = docSnap.data().invoice_date;
+        const vendorName = docSnap.data().vendor_name;
+        const vendorPhone = docSnap.data().vendor_phone;
+        const invoiceTotal = docSnap.data().total;
+        const invoiceItems = docSnap.data().items;
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
 }
